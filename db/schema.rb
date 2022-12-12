2788aa05eb33414726d493194a00ae0089dd4461
yuliatokaryk <julia4099@gmail.com>
2022-12-12 11:21:34 +0200

    questions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_12_091711) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_13_130934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.bigint "sender_id", null: false
+    t.bigint "recipient_id", null: false
+    t.string "content"
+    t.boolean "public"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipient_id"], name: "index_questions_on_recipient_id"
+    t.index ["sender_id"], name: "index_questions_on_sender_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_12_091711) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "questions", "users", column: "recipient_id"
+  add_foreign_key "questions", "users", column: "sender_id"
 end
